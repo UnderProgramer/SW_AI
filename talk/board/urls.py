2209aee21a5f4c01f13e40f3b2b6d1e408908a14
UnderProgramer <ajdjskq13324@gmail.com>
2025-09-22 23:35:39 +0900

    urls

--- talk/board/urls.py
+++ talk/board/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "talk.board"
 
 urlpatterns = [
-    path('/', views.CreateBoardView.as_view()),
+    path('', views.CreateBoardView.as_view()),
     path('view', views.ViewBoard.as_view()),
     path('view/<int:board_id>', views.OnceViewBoard.as_view()),
 ]
